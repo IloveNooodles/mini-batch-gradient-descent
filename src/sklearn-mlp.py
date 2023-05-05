c2e7b20645b0ec4e7cdb14caa7424c36fed8e1c0
IloveNooodles <mgarebaldhie80@gmail.com>
2023-05-05 17:26:43 +0700

    feat: adding one hot encoding

--- src/sklearn-mlp.py
+++ src/sklearn-mlp.py
@@ -4,6 +4,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import (GridSearchCV, cross_validate,
                                      train_test_split)
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder
 
 
 def predict_mlp():
@@ -17,6 +18,13 @@ def predict_mlp():
 
     iris = load_iris()
     x, y = iris.data, iris.target
+
+    # Encode dataset
+    encoder = OneHotEncoder(sparse_output=False)
+    y_reshaped = y.reshape(len(y), 1)
+    y = encoder.fit_transform(y_reshaped)
+
+    # Split dataset
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=0.2, random_state=100)
 
@@ -26,31 +34,31 @@ def predict_mlp():
     print("X Test: ", x_test)
     print("Y Test: ", y_test)
 
-    mlp = MLPClassifier(
-        # hidden_layer_sizes=(3, 3),
-        activation="identity",
-        solver="sgd",
-        batch_size=200,
-        learning_rate="invscaling",
-        learning_rate_init=0.2,
-        power_t=0.5,
-        max_iter=200,
-        shuffle=True,
-        random_state=100,
-        verbose=True,
-        tol=1e-6,
-        momentum=0.6,
-    )
+    # mlp = MLPClassifier(
+    #     # hidden_layer_sizes=(3, 3),
+    #     activation="identity",
+    #     solver="sgd",
+    #     batch_size=200,
+    #     learning_rate="invscaling",
+    #     learning_rate_init=0.2,
+    #     power_t=0.5,
+    #     max_iter=200,
+    #     shuffle=True,
+    #     random_state=100,
+    #     verbose=True,
+    #     tol=1e-6,
+    #     momentum=0.6,
+    # )
 
-    mlp.fit(x_train, y_train)
+    # mlp.fit(x_train, y_train)
 
-    print("=" * 8 + " Result fit " + "="*8)
-    # for key in dir(mlp):
-    #     print("Key: ", key, "\nValue: ", mlp.__getattribute__(key))
+    # print("=" * 8 + " Result fit " + "="*8)
+    # # for key in dir(mlp):
+    # #     print("Key: ", key, "\nValue: ", mlp.__getattribute__(key))
 
-    res = mlp.predict(x_test)
-    print(classification_report(y_test, res))
-    print(confusion_matrix(y_test, res))
+    # res = mlp.predict(x_test)
+    # print(classification_report(y_test, res))
+    # print(confusion_matrix(y_test, res))
 
 
 if __name__ == "__main__":
